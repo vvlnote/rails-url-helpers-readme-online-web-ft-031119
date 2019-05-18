0adb492c24f46651561dbecf0a4aaaa0cabd0911
Joy Lin <vvlnote@hotmail.com>
2019-05-18 22:40:00 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
     Post.create(title: "My Post", description: "My post desc")
+    Post.create(title: "My Title", description: "My post description")
